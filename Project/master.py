8d4f7e2f839038cc1441747b14eebcb550c1d073
Lily <lykavanagh1@gmail.com>
2020-06-18 17:24:46 +0100

    Working plot with pixels in blue

--- Project/master.py
+++ Project/master.py
@@ -6,12 +6,27 @@ Created on Fri Jun 12 13:02:23 2020
 @author: cormac
 """
 
-#Importing All of the relevant modules
+# Importing All of the relevant modules
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 
 
 
+
+# Global Constants
+N_EVENTS = 2500
+TEL_PIXEL_COUNT = 499
+
+
+
+
+
+
+
+
+
+# For Now this class is not used. Leaving it here for further use.
 class PMPIX():
 
     def __init__(self, tele_id, pix_id, pix_shape, angle, x_pos, y_pos, pmt_radius, geom_eff, quant_eff, sig_fluct, sig_rize, sig_fall, rc_coupling, belong_bol, anal_bol, time_offset, gain_factor):
@@ -41,50 +56,91 @@ class PMPIX():
         print(self.x_pos, self.y_pos)
 
 
-
+# Test for class
 PMPIX1= PMPIX(1, 1, 1, 0.0, 0.00, 0.00, 14.0, 1.00, 1, 0.44, 4.0, 65.0, 0.000, 1, 1, 0.0, 1.0)
 
 
 
-PMPIX1.print_pos()
-
-read_PMT_info = open("PMTS_info.txt", "r")
-contents = read_PMT_info.read()
-new = contents.split("\n")
-del new[-1]
 
 
-x_array = np.zeros(len(new))
-y_array = np.zeros(len(new))
 
 
-for i in range(len(new)):
-    # j = new[i].partition(" ")[2]
-    new[i] = new[i].split()
-    new[i][0] = new[i][0] + new[i][1] + "_" + new[i][2]
-    del new[i][1:3]
-    #print(new[i])
-    x_array[i] = new[i][3]
-    y_array[i] = new[i][4]
 
-    if i==25 or i==26:
-        pix_col='red'
+
+# Reading our Telescope data
+read_tel_details = open("data/PMTS_info.txt", "r")
+pmts_info = read_tel_details.read()
+tel_details = pmts_info.splitlines()
+
+
+# Reading our Event data
+read_event_details = open("data/gamsims_2500.dat")
+gamsims = read_event_details.read()
+events = gamsims.splitlines(0)
+
+
+# Restructures Data into a 2D Array[ [event_1] [event_2] ... ] with [ pix_1_val pix_2_val ... ]
+events_array = np.zeros((2500, 499))
+for count, element in enumerate(events):
+    element = element.split()
+    events_array[int(element[0]) - 1][int(element[1]) - 1] = int(element[2])
+
+#print(events_array)
+
+
+
+
+
+
+def PixelDetection(pix_num):
+    if pix_num >= 20:
+        return 'blue'
     else:
-        pix_col='grey'
-    plt.plot(x_array[i], y_array[i], 'h', color=pix_col, markersize=8)
-    #plt.text(x_array[i], y_array[i], i)   # Adds pixel number to plot
-
-
-
-plt.title("PMPIX")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.show()
-
-#plt.plot(x_array, y_array, 'h', color='grey')
-#plt.show()
+        return 'grey'
 
 
 
 
 
+def TelescopePlot(current_event, index):
+
+    x_array = np.zeros(len(tel_details))
+    y_array = np.zeros(len(tel_details))
+
+    for i in range(len(tel_details)):
+        # Gets the telescope Details and coordinates for pixels, and plots them according to PixelDetection
+
+        # j = tel_details[i].partition(" ")[2]
+
+        tel_details[i] = tel_details[i].split()
+        tel_details[i][0] = tel_details[i][0] + tel_details[i][1] + "_" + tel_details[i][2]
+        del tel_details[i][1:3]
+        #print(tel_details[i])
+        x_array[i] = tel_details[i][3]
+        y_array[i] = tel_details[i][4]
+
+
+        pix_col=PixelDetection(current_event[i])
+
+
+        plt.plot(x_array[i], y_array[i], 'h', color=pix_col, markersize=8)
+        #plt.text(x_array[i], y_array[i], i)   # Adds pixel number to plot
+
+
+
+
+
+    # Finishing the Plot for the telescope
+    plt.title("PMPIX")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.savefig('output/output_plot_event_{0}.pdf'.format(index + 1), bbox_inches='tight')
+
+
+
+
+for count, element in enumerate(events_array):
+    TelescopePlot(element, count)
+    print(count)
+    print(element)
+    exit()
